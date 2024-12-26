356e57f523fd47ff0dd131f4b22b9dc622fec8a2
hassan_afzal_31g <hassan.afzal@31g.co.uk>
2024-12-26 12:01:25 +0500

    halaluea

--- src/App.js
+++ src/App.js
@@ -9,17 +9,19 @@ function App() {
   console.log("location: ", location);
   const getNewPage = async () => {
     try {
-      // I want  to send body as well with this request
-      const res = await fetch(
-        `https://dev-backend-formbuilder.31g.co.uk/page/render?appId=${62}&path=${location.pathname.slice(1)}`,
+      // I want  to send body as well with this request <- OK
+      let res = await fetch(
+        `https://dev-backend-formbuilder.31g.co.uk/page/render?token=${"ctmfj8afvq6idkll45rg"}&path=${location.pathname.slice(
+          1
+        )}`,
         {
           method: "POST",
-          body : JSON.stringify({})
+          body: JSON.stringify({}),
         }
       );
-
-      setForm(res.data.form);
+      res = await res.json();
       console.log("res: ", res);
+      setForm(JSON.parse(res?.data?.form));
     } catch (err) {
       console.error(err);
     }
@@ -28,6 +30,7 @@ function App() {
     getNewPage();
   }, [location.pathname]);
 
+  console.log('form:', form);
   return (
     <div>
       <FormPreviewer form={form} dataDictionary={{}} />
